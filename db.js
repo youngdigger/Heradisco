938a654ee91a998bdb25d1b06e566d33a8b8913c
Giovanni <gigialf2006@gmail.com>
2024-09-07 20:20:31 -0500

    Conexión a PostgreSQL configurada

--- db.js
+++ db.js
@@ -1,19 +1,11 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
 
-const client = new Client({
+const pool = new Pool({
     connectionString: connectionString,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
-client.connect(err => {
-    if (err) {
-        console.error('Error al conectar a PostgreSQL:', err.stack);
-    } else {
-        console.log('Conectado a PostgreSQL.');
-    }
-});
-
-module.exports = client;
+module.exports = pool;
--- reservas.sql
+++ reservas.sql
@@ -1,20 +1,20 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "reservas" (
-	" id"	INTEGER,
-	"nombre"	TEXT NOT NULL,
-	"email"	TEXT NOT NULL,
-	"fecha"	TEXT NOT NULL,
-	"personas"	INTEGER NOT NULL,
-	PRIMARY KEY(" id" AUTOINCREMENT)
+DROP TABLE IF EXISTS reservas;
+
+CREATE TABLE reservas (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    email TEXT NOT NULL,
+    fecha DATE NOT NULL,
+    hora INTEGER NOT NULL
 );
-INSERT INTO reservas VALUES(1,'ricardo','heradisco@gmail.com','2024-09-09',5);
-INSERT INTO reservas VALUES(2,'ricardo','heradisco@gmail.com','2024-09-16',5);
-INSERT INTO reservas VALUES(3,'sebas','heradisco@gmail.com','2024-09-26',6);
-INSERT INTO reservas VALUES(4,'ricardo','heradisco@gmail.com','2024-09-10',5);
-INSERT INTO reservas VALUES(5,'ricardo','heradisco@gmail.com','2024-09-16',5);
-INSERT INTO reservas VALUES(6,'ricardo','heradisco@gmail.com','2024-09-16',5);
-INSERT INTO reservas VALUES(7,'ana','rita@gmail.com','2024-09-23',-6);
-DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('reservas',7);
+
+-- Inserción de datos
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (1, 'ricardo', 'heradisco@gmail.com', '2024-09-09', 5);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (2, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (3, 'sebas', 'heradisco@gmail.com', '2024-09-26', 6);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (4, 'ricardo', 'heradisco@gmail.com', '2024-09-10', 5);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (5, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (6, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (7, 'ana', 'rita@gmail.com', '2024-09-23', -6);
+
 COMMIT;
