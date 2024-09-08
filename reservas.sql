de1cb8efc5fd843b303485cb2d643fc5104ee7f6
Giovanni <gigialf2006@gmail.com>
2024-09-08 09:00:58 -0500

    Corrección de estructura y configuración de base de datos

--- reservas.sql
+++ reservas.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS reservas;
-(
+DROP TABLE IF EXISTS reservas;(
+
 
 CREATE TABLE reservas (
   id SERIAL PRIMARY KEY,
@@ -13,12 +13,12 @@ CREATE TABLE reservas (
 );
 
 -- Inserción de datos
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (1, 'ricardo', 'heradisco@gmail.com', '2024-09-09', 5);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (2, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (3, 'sebas', 'heradisco@gmail.com', '2024-09-26', 6);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (4, 'ricardo', 'heradisco@gmail.com', '2024-09-10', 5);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (5, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (6, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
-INSERT INTO reservas (id, nombre, email, fecha, hora) VALUES (7, 'ana', 'rita@gmail.com', '2024-09-23', -6);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (1, 'ricardo', 'heradisco@gmail.com', '2024-09-09', 5);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (2, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (3, 'sebas', 'heradisco@gmail.com', '2024-09-26', 6);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (4, 'ricardo', 'heradisco@gmail.com', '2024-09-10', 5);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (5, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (6, 'ricardo', 'heradisco@gmail.com', '2024-09-16', 5);
+INSERT INTO reservas (id, nombre, email, fecha, ) VALUES (7, 'ana', 'rita@gmail.com', '2024-09-23', -6);
 
 COMMIT;
